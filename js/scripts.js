b3b95b92e2c5622c8ec2c7e5dda7e03cad8a8e2b
Tayyab <tayyabnasir22@gmail.com>
2023-07-06 17:55:25 +0500

    formatting changes

--- js/scripts.js
+++ js/scripts.js
@@ -154,10 +154,10 @@ window.addEventListener('DOMContentLoaded', event => {
         sourceHTML = `<div class="d-flex flex-column flex-md-row justify-content-between mb-2">
                         <div class="flex-grow-1">
                             <h3 class="mb-0"><a href="${projectDetailsLink + element.details}">${element.name}</a></h3>
-                            <div class="subheading mb-3">${element.company}</div>
+                            <div class="subheading mb-2">${element.company}</div>
                         </div>
-                        <div class="flex-shrink-0"><span class="text-primary">${element.year}</span></div>
-                    </div>`
+                        <div class="flex-shrink-0 mb-5"><span class="text-primary">${element.year}</span></div>
+                    </div><hr/>`
         container.innerHTML += sourceHTML;
     }); 
 
